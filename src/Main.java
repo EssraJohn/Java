e845e2aad6dfaf2eeeb5d47663b228c6276829bd
EssraJohn <essraj05@gmail.com>
2022-09-15 07:29:46 +0530

    Java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Box mybox= new Box();
+        mybox.width= 2;
+        mybox.depth= 5;
+        mybox.height = 3;
+        double vol;
+        vol= mybox.depth * mybox.height * mybox.width;
+        System.out.println("Vol is:" +vol);
     }
 }
